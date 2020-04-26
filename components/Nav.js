2ff2e18e68495c2457d74896ebc68d8f34ae0091
Anita Kowalska <anita.kniaziew@gmail.com>
2020-04-26 18:55:34 +0200

    Add a components scroll

--- components/Nav.js
+++ components/Nav.js
@@ -1,10 +1,36 @@
-export default function Nav() {
-  return (
-    <div>
-      <li>About</li>
-      <li>Projects</li>
-      <li>CV</li>
-      <li>Contact</li>
-    </div>
-  );
+import React from "react";
+
+export default class Nav extends React.Component {
+  constructor(props) {
+    super(props);
+    this.aboutRef = React.createRef();
+    this.contactRef = React.createRef();
+    this.projectsRef = React.createRef();
+    this.cvRef = React.createRef();
+    this.scroll = this.scroll.bind(this);
+  }
+
+  scroll() {
+    console.log("click");
+    this.aboutRef.current.scrollIntoView();
+  }
+
+  render() {
+    return (
+      <div>
+        <li ref={this.aboutRef} onClick={this.scroll}>
+          About
+        </li>
+        <li ref={this.projectsRef} onClick={this.scroll}>
+          Projects
+        </li>
+        <li ref={this.cvRef} onClick={this.scroll}>
+          CV
+        </li>
+        <li ref={this.contactRef} onClick={this.scroll}>
+          Contact
+        </li>
+      </div>
+    );
+  }
 }
