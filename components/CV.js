03d499c0ea64ae7b3b91696bb57a28620834d022
Anita Kowalska <anita.kniaziew@gmail.com>
2020-05-02 21:17:40 +0200

    Change CV layout

--- components/CV.js
+++ components/CV.js
@@ -14,71 +14,71 @@ export default function CV(props) {
   return (
     <div className="section" ref={props.reference}>
       <h1>Experience &amp; Education</h1>
-      <div className={styles.midsection}>
-        <h2 className={styles.header}>Experience</h2>
-        <div>
+      <div className={styles.cvBox}>
+        <div className={styles.midsection}>
+          <h2 className={styles.header}>Experience</h2>
           <p className={styles.data}>2016 - 2018</p>
-          <p>Accountant at Tax Care S.A.</p>
+          <p>Accountant</p>
+          <p className={styles.place}>Tax Care S.A.</p>
         </div>
-      </div>
-      <div className={styles.midsection}>
-        <h2 className={styles.header}>Education</h2>
-        <div>
+        <div className={styles.midsection}>
+          <h2 className={styles.header}>Education</h2>
           <p className={styles.data}>2012 - 2014</p>
-          <p>
-            Master of Finance and Accounting, University of Economics in
-            Katowice
-          </p>
+          <p>Master of Finance and Accounting</p>
+          <p className={styles.place}>University of Economics in Katowice</p>
         </div>
-      </div>
-      <div className={styles.midsection}>
-        <h2 className={styles.header}>Skills</h2>
-        <ul className={styles.skills}>
-          <li className={styles.technology}>
-            <FontAwesomeIcon
-              icon={faJsSquare}
-              className={styles.technologyIcon}
-            />
-            JavaScript
-          </li>
-          <li className={styles.technology}>
-            <FontAwesomeIcon icon={faHtml5} className={styles.technologyIcon} />
-            HTML5
-          </li>
-          <li className={styles.technology}>
-            <FontAwesomeIcon
-              icon={faCss3Alt}
-              className={styles.technologyIcon}
-            />
-            CSS
-          </li>
-          <li className={styles.technology}>
-            <FontAwesomeIcon
-              icon={faGitSquare}
-              className={styles.technologyIcon}
-            />
-            Git
-          </li>
-          <li className={styles.technology}>
-            <FontAwesomeIcon
-              icon={faPython}
-              className={styles.technologyIcon}
-            />
-            Python
-          </li>
-          <li className={styles.technology}>
-            <FontAwesomeIcon
-              icon={faTerminal}
-              className={styles.technologyIcon}
-            />
-            Unix Shell
-          </li>
-        </ul>
-      </div>
-      <div className={styles.midsection}>
-        <h2 className={styles.header}>Languages</h2>
-        <li>English B2</li>
-        <li>Japanese </li>
+        <div className={styles.midsection}>
+          <h2 className={styles.header}>Skills</h2>
+          <ul className={styles.skills}>
+            <li className={styles.technology}>
+              <FontAwesomeIcon
+                icon={faJsSquare}
+                className={styles.technologyIcon}
+              />
+              JavaScript
+            </li>
+            <li className={styles.technology}>
+              <FontAwesomeIcon
+                icon={faHtml5}
+                className={styles.technologyIcon}
+              />
+              HTML5
+            </li>
+            <li className={styles.technology}>
+              <FontAwesomeIcon
+                icon={faCss3Alt}
+                className={styles.technologyIcon}
+              />
+              CSS
+            </li>
+            <li className={styles.technology}>
+              <FontAwesomeIcon
+                icon={faGitSquare}
+                className={styles.technologyIcon}
+              />
+              Git
+            </li>
+            <li className={styles.technology}>
+              <FontAwesomeIcon
+                icon={faPython}
+                className={styles.technologyIcon}
+              />
+              Python
+            </li>
+            <li className={styles.technology}>
+              <FontAwesomeIcon
+                icon={faTerminal}
+                className={styles.technologyIcon}
+              />
+              Unix Shell
+            </li>
+          </ul>
+        </div>
+        <div className={styles.midsection}>
+          <h2 className={styles.header}>Languages</h2>
+          <li>English B2</li>
+          <li>Japanese </li>
+        </div>
       </div>
     </div>
   );
