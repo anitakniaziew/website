f52db37a854301afcb350a833b577a0d7698e238
Anita Kowalska <anita.kniaziew@gmail.com>
2020-05-02 23:08:20 +0200

    Add github repositories link

--- components/Project.js
+++ components/Project.js
@@ -1,6 +1,8 @@
 import { render } from "react-dom";
 import Box from "./Box.js";
 import Tech from "./Tech.js";
+import data from "../data.js";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./Project.module.css";
 
 export default class Project extends React.Component {
@@ -8,10 +10,19 @@ export default class Project extends React.Component {
     return (
       <Box>
         <div className={styles.project}>
-          <img src={this.props.img} className={styles.projectImg} />
+          <a href={this.props.href} target="_blank">
+            <img src={this.props.img} className={styles.projectImg} />
+          </a>
           <div className={styles.details}>
             <h2>{this.props.name}</h2>
             <p>{this.props.description}</p>
+            <a href={this.props.git} target="_blank">
+              <FontAwesomeIcon
+                icon={data.git.icon}
+                className={styles.gitIcon}
+              ></FontAwesomeIcon>
+              anitakniaziew/{this.props.name}
+            </a>
             <ul className={styles.techList}>
               {this.props.stacks.map((tech) => (
                 <Tech key={tech.name} name={tech.name} icon={tech.icon}></Tech>
--- components/Projects.js
+++ components/Projects.js
@@ -7,32 +7,42 @@ export default function Projects(props) {
       <h1>Projects</h1>
       <Project
         name="Kotoba"
+        href="https://app.kotoba.dev"
         img="./kotoba.png"
         description="Simple aplication for learning japanese words"
+        git="https://github.com/anitakniaziew/Kotoba"
         stacks={[tech.react, tech.js, tech.css, tech.html]}
       ></Project>
       <Project
         name="warsaw.ex"
+        href="https://www.warsawex.org/"
         img="./warsaw-ex.png"
         description="Warsaw elixir meet-up webpage"
+        git="https://github.com/anitakniaziew/Kotoba"
         stacks={[tech.react, tech.js, tech.css, tech.html]}
       ></Project>
       <Project
         name="Noughts and Crosses"
+        href="https://anitakniaziew.github.io/Noughts-and-Crosses/noughtsCrosses.html"
         img="./ox.png"
         description="That's a neon style simple noughts and crosses game. Created to check out CSS and JS skills without using any tutorials, just following ideas and intuition."
+        git="https://github.com/anitakniaziew/Noughts-and-Crosses"
         stacks={[tech.js, tech.css, tech.html]}
       ></Project>
       <Project
         name="Memory"
+        href="https://anitakniaziew.github.io/memory/index.html"
         img="./memory.png"
         description="Just a simple memory game. Created to excercise CSS and JS skills."
+        git="https://github.com/anitakniaziew/memory"
         stacks={[tech.js, tech.css, tech.html]}
       ></Project>
       <Project
         name="Calculator"
+        href="https://anitakniaziew.github.io/calculator/calculator.html"
         img="./calculator.png"
         description="Basic operations calculator."
+        git="https://github.com/anitakniaziew/calculator"
         stacks={[tech.js, tech.css, tech.html]}
       ></Project>
     </div>
