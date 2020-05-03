2cf8ee41dc7a218d48d0c2d3f3d116dbbbe4490c
Anita Kowalska <anita.kniaziew@gmail.com>
2020-05-03 23:27:23 +0200

    Add title and description meta-tags

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Head from "next/head";
+
 import Header from "../components/Header.js";
 import NavItem from "../components/NavItem.js";
 import About from "../components/About.js";
@@ -24,27 +26,18 @@ export default class Home extends React.Component {
   render() {
     return (
       <div className="container">
+        <Head>
+          <title>Anita Kowalska - JavaScript, React, CSS and HTML</title>
+          <meta
+            name="description"
+            content="I'm an aspiring JavaScript and React developer - check out my project portfolio and GitHub repositories."
+          />
+        </Head>
         <Header>
-          <NavItem
-            // name="About"
-            reference={this.aboutRef}
-            icon={faUser}
-          ></NavItem>
-          <NavItem
-            // name="Projects"
-            reference={this.projectsRef}
-            icon={faClone}
-          ></NavItem>
-          <NavItem
-            // name="CV"
-            reference={this.cvRef}
-            icon={faBriefcase}
-          ></NavItem>
-          <NavItem
-            // name="Contact"
-            reference={this.contactRef}
-            icon={faPaperPlane}
-          ></NavItem>
+          <NavItem reference={this.aboutRef} icon={faUser}></NavItem>
+          <NavItem reference={this.projectsRef} icon={faClone}></NavItem>
+          <NavItem reference={this.cvRef} icon={faBriefcase}></NavItem>
+          <NavItem reference={this.contactRef} icon={faPaperPlane}></NavItem>
         </Header>
         <main>
           <About reference={this.aboutRef} />
